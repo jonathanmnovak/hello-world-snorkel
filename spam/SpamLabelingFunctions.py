ddaa49f8eef584b856fb06c50f24a79ffd57da60
jsupanova <jonathan.m.novak@gmail.com>
2021-08-19 14:26:31 -0500

    Add regex LF and evaluate performance

--- spam/SpamLabelingFunctions.py
+++ spam/SpamLabelingFunctions.py
@@ -1,4 +1,5 @@
 from snorkel.labeling import labeling_function
+import re
 
 #labels
 ABSTAIN = -1
@@ -25,3 +26,13 @@ def check_out(x:str)->int:
     :return: 1 if the phrase "check out" is in the text else -1
     """
     return SPAM if "check out" in x.text.lower() else ABSTAIN
+
+@labeling_function()
+def regex_check_out(x:str)->int:
+    """
+    Labeling function which classifies a piece of text as spam if it contains
+    the the words "check" and "out" with any set of characters in between.
+    :param x: Text to evaluate
+    :return: 1 if the condition is met else -1
+    """
+    return SPAM if re.search(r"check.*out", x.text, flags=re.I) else ABSTAIN
